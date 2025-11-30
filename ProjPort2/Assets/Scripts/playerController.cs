f2e670c4db9ad0d0bc66e6eabc9a8507f313062d
Zachary Brodahl <zacharybrodahl@gmail.com>
2025-11-30 17:34:17 -0500

    player movement implemented

--- ProjPort2/Assets/Scripts/playerController.cs
+++ ProjPort2/Assets/Scripts/playerController.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+
+    [Range (1, 10)][SerializeField] int speed;
+
+    Vector3 moveDir;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,12 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
+    }
+
+    void movement()
+    {
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir.normalized * speed * Time.deltaTime);
     }
 }

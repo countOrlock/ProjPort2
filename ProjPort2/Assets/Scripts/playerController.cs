83d4f233ac7fbc34fe59f8ec0895e55a5c47b121
Zachary Brodahl <zacharybrodahl@gmail.com>
2025-12-03 11:46:11 -0500

    sprinting and jumping added

--- ProjPort2/Assets/Scripts/playerController.cs
+++ ProjPort2/Assets/Scripts/playerController.cs
@@ -4,15 +4,28 @@ public class playerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
 
-    [Range (1, 10)][SerializeField] int speed;
+    [Range (1, 10)][SerializeField] int wSpeed;
+    [Range (1, 10)][SerializeField] int rSpeed;
+    [Range (1, 20)][SerializeField] int jumpSpeed;
+    [SerializeField] float gravity;
+    [SerializeField] int jumpCount;
 
     Vector3 moveDir;
 
+    Vector2 walkDir;
+
+    float jumpMod;
+
+    int speedMod;
+
+    int maxJump;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        jumpMod = 0f;
+        speedMod = wSpeed;
+        maxJump = jumpCount;
     }
 
     // Update is called once per frame
@@ -23,7 +36,37 @@ public class playerController : MonoBehaviour
 
     void movement()
     {
-        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-        controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        //jumping and gravity
+
+        if (!controller.isGrounded)
+        {
+            jumpMod = jumpMod - gravity;
+        }
+        else 
+        {
+            jumpMod = 0;
+            jumpCount = maxJump;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpMod = jumpSpeed;
+            jumpCount--;
+        }
+
+        //sprinting
+        if (Input.GetButton("Sprint"))
+        {
+            speedMod = rSpeed;
+        }
+        else
+        {
+            speedMod = wSpeed;
+        }
+
+        //movement execution
+        walkDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        moveDir = walkDir.x * transform.right * speedMod + walkDir.y * transform.forward * speedMod + jumpMod * transform.up;
+        controller.Move(moveDir * Time.deltaTime);
     }
 }

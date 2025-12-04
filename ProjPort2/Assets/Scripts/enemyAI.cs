8ebd6ef554c8b8856795841a184e0b8e367d2ba0
Dante <dantevchacon@gmail.com>
2025-12-04 02:05:07 -0500

    Added 2 enemies with unique NavMesh movements

---
+++ ProjPort2/Assets/Scripts/enemyAI.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class enemyAI : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+
+    [SerializeField] int HP;
+    [SerializeField] bool scaredOfPlayer;
+
+    GameObject player;
+
+    bool playerInRange;
+
+    Vector3 playerDir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && !scaredOfPlayer)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        else if (playerInRange && scaredOfPlayer)
+        {
+            playerDir = player.transform.position - transform.position;
+
+            Debug.Log(playerDir);
+
+            float xVectorSwapNum = 2 / Mathf.Abs(playerDir.x) + 1;
+            float zVectorSwapNum = 2 / Mathf.Abs(playerDir.z) + 1;
+
+            float oppositePlayerX = player.transform.position.x - (xVectorSwapNum * playerDir.x);
+            float oppositePlayerZ = player.transform.position.z - (zVectorSwapNum * playerDir.z);
+            agent.SetDestination(new Vector3(oppositePlayerX, transform.position.y, oppositePlayerZ));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}

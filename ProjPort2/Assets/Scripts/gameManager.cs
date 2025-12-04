99e8d9f3e91e5a6c40a879d9cbc4e70001e11179
Anjelique De Freitas <anjelique.de@gmail.com>
2025-12-04 13:17:07 -0500

    UI created and gameManager added with the Pause menu currently showing up when 'esc' is pressed

---
+++ ProjPort2/Assets/Scripts/gameManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public GameObject player;
+    public playerController playerScript;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        timeScaleOrig = Time.timeScale;
+
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = false;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+}

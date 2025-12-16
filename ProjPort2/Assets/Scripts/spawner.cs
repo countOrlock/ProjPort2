acdb98fc3c07ee11dfa036ded9c981126d739601
Abram Quintana <abram02242006@gmail.com>
2025-12-16 15:10:58 -0500

    Added spawner script and spawner object

---
+++ ProjPort2/Assets/Scripts/spawner.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] GameObject objectToSpawn;
+    [SerializeField] int spawnAmount;
+    [SerializeField] int spawnRate;
+
+    int spawnCount;
+    float spawnTimer;
+
+    bool startSpawning;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager.instance.updateGameGoal(spawnAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startSpawning)
+        {
+            spawnTimer += Time.deltaTime;
+
+            if (spawnCount < spawnAmount && spawnTimer >= spawnRate)
+            {
+                spawn();
+            }
+        }
+    }
+
+    void spawn()
+    {
+        spawnTimer = 0;
+        spawnCount++;
+        Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            startSpawning = true;
+        }
+    }
+}

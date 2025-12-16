ee8c876c7be384ea52746344a44d38865ba3292f
Anjelique De Freitas <anjelique.de@gmail.com>
2025-12-16 14:41:21 -0500

    saving my changes so they're not lost

--- ProjPort2/Assets/Scripts/questInfo.cs
+++ ProjPort2/Assets/Scripts/questInfo.cs
@@ -10,10 +10,4 @@ public class questInfo : ScriptableObject
     public string questObjective;
     [Range(0, 10)] public int itemsForQuest; // lowest is 0 for now for testing purposes till the quest system if fully set up
     [Range (0, 2)] public int questStatus;
-    public enum questID
-    {
-        Completed = 1,
-        In_Progress, // 2
-        Not_Accepted // 3
-    }
 }

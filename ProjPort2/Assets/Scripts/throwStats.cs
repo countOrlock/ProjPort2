8e9b39a97e1c1f591c2269a35a4964fbf467fbd2
Zachary Brodahl <zacharybrodahl@gmail.com>
2026-01-13 18:42:24 -0500

    added throwStats scriptable object

---
+++ ProjPort2/Assets/Scripts/throwStats.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class throwStats : ScriptableObject
+{
+    public GameObject projectile = null;
+
+    public int ammoCurr;
+    [Range(1, 50)] public int ammoMax;
+
+    public AudioClip[] throwSound;
+    public AudioClip[] pickupSound;
+    [Range(0, 1)] public float throwSoundVol;
+    [Range(0, 1)] public float pickupSoundVol;
+}

1e03bd7a6cd0fcc9fba96357937ba6bf06ca4597
Dante <dantevchacon@gmail.com>
2026-01-15 16:48:18 -0500

    NPC Manager Finds Objects in scene with 'Spawner' tag

--- ProjPort2/Assets/Scripts/NPCs/NPCManager.cs
+++ ProjPort2/Assets/Scripts/NPCs/NPCManager.cs
@@ -1,7 +1,28 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class NPCManager : MonoBehaviour
 {
+    [SerializeField] GameObject smallGame1;
+    [SerializeField] GameObject smallGame2;
+    [SerializeField] GameObject mediumGame1;
+    [SerializeField] GameObject mediumGame2;
+    [SerializeField] GameObject mediumHostileGame;
+    [SerializeField] GameObject bigGame;
+
+    [SerializeField] int smallGame1SpawnLimit;
+    [SerializeField] int smallGame2SpawnLimit;
+    [SerializeField] int mediumGame1SpawnLimit;
+    [SerializeField] int mediumGame2SpawnLimit;
+    [SerializeField] int mediumHostileGameSpawnLimit;
+
+    GameObject[] spawners;
+
+    void Awake()
+    {
+        spawners = GameObject.FindGameObjectsWithTag("Spawner");
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +34,5 @@ public class NPCManager : MonoBehaviour
     {
         
     }
+
 }
